248cc13fe5ead4d18e98fde97680b65209613ca0
thakur vishal singh <vishalrajput0246@gmail.com>
2025-02-06 13:03:24 -0500

    sum file added

---
+++ sum.java
@@ -0,0 +1,11 @@
+public class {
+public static void main(string[]arge){
+
+System.out.println("Hello world");
+int a=5;
+int b = 10;
+int sum = a+b;
+System.out.println("the sum of two is :::: "+sum);
+}
+
+}
